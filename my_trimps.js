043e1099d3ddec295cfa215ed5c90fc1395344a9
LordJimX <103373081+LordJimX@users.noreply.github.com>
2022-11-19 19:26:56 +0100

    Update my_trimps.js

--- my_trimps.js
+++ my_trimps.js
@@ -393,9 +393,6 @@ function DG_auto_upgrade(){
     debug("Start buying DG upgrade");
     tooltip("Upgrade Generator", null, "update");
     AutoDGInterval = setInterval(buy_next_DG_upgrade, AutoDGIntervalTime);
-    document.getElementById("natureA").click();
-    AutoNatureInterval = setInterval(buy_next_nature_upgrade, AutoNatureIntervalTime);
-    document.getElementById("allA").click();
 }
 
 function buy_next_nature_upgrade(){
@@ -412,6 +409,7 @@ function buy_next_nature_upgrade(){
         naturePurchase('upgrade', 'Ice');
     }
     clearInterval(AutoNatureInterval);
+    document.getElementById("allA").click();
 }
 
 function buy_next_DG_upgrade(){
@@ -455,6 +453,8 @@ function buy_next_DG_upgrade(){
         debug('No more DG upgrade affordable');
         clearInterval(AutoDGInterval);
         cancelTooltip();
+        document.getElementById("natureA").click();
+        AutoNatureInterval = setInterval(buy_next_nature_upgrade, AutoNatureIntervalTime);
     }
 }
 
